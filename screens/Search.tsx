256793af999320f0ff3cbc6cec1dd178425601aa
r9988.2588 <r9988.2588@gmail.com>
2024-09-17 12:11:12 +0000

    added comments to the search

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,12 +1,16 @@
 import { StyleSheet, Text, View, SafeAreaView, Pressable } from 'react-native';
+// Imports the usenavigation hook
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
+// Icons
 import AntDesign from '@expo/vector-icons/AntDesign';
 
 const Search = () => {
 
+    // Initializes the usenavigation hook
     const navigation = useNavigation();
 
+    // This function navigates to the previous screen Home
     const back = () => {
         navigation.goBack();
         console.log("Just went back from the Search screen");
@@ -15,6 +19,7 @@ const Search = () => {
     return (
         <SafeAreaView style={styles.background}>
 
+            {/* This is the back button, which navigates back to the home */}
             <Pressable onPress={back}>
                 <AntDesign name="left" size={24} color="white" style={{ marginTop: 15, marginLeft: 15 }} />
             </Pressable>
