96d99a3a85b00d7a11174d82677a8c8d2f8074bf
r9988.2588 <r9988.2588@gmail.com>
2024-09-20 08:45:07 +0000

    removed the styles in the app.tsx

--- App.tsx
+++ App.tsx
@@ -118,18 +118,3 @@ const App = () => {
 }
 
 export default App;
-
-// Stylings
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  }
-})
